3797acef08f8139b6d6d2a854721c9c80925b6da
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-08-16 22:11:47 +0500

    changed content field

--- schemas/blog.ts
+++ schemas/blog.ts
@@ -10,11 +10,34 @@ export default {
     },
     {
       name: 'content',
-      type: 'array',
       title: 'Content',
+      type: 'array',
       of: [
         {
           type: 'block',
+          marks: {
+            decorators: [
+              // ...
+            ],
+            annotations: [
+              {
+                name: 'internalLink',
+                type: 'object',
+                title: 'Internal link',
+                fields: [
+                  {
+                    name: 'reference',
+                    type: 'reference',
+                    title: 'Reference',
+                    to: [
+                      {type: 'post'},
+                      // other types you may want to link to
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
         },
       ],
     },
