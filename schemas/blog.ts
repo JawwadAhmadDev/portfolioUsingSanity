768d2b63e7183f6a2e6c6516e589922e52db1f60
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-08-16 22:39:59 +0500

    blog image added in schema

--- schemas/blog.ts
+++ schemas/blog.ts
@@ -3,31 +3,50 @@ export default {
   type: 'document',
   title: 'Blog',
   fields: [
+    // name
     {
       name: 'name',
       type: 'string',
       title: 'Name',
     },
+    // content
     {
-        name: 'content',
-        title: 'Content',
-        type: 'array',
-        of: [
-          {
-            type: 'block',
-            marks: {
-              decorators: [
-                {title: 'Strong', value: 'strong'},
-                {title: 'Emphasis', value: 'em'},
-                {title: 'Code', value: 'code'}
-              ]
-            }
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          marks: {
+            decorators: [
+              {title: 'Strong', value: 'strong'},
+              {title: 'Emphasis', value: 'em'},
+              {title: 'Code', value: 'code'},
+            ],
           },
+        },
+        {
+          type: 'image',
+        },
+      ],
+    },
+    // blog image
+    {
+        title: 'Blog Image',
+        name: 'blogImage',
+        type: 'image',
+        options: {
+          hotspot: true // <-- Defaults to false
+        },
+        fields: [
           {
-            type: 'image'
-          }
+            name: 'caption',
+            type: 'string',
+            title: 'Caption',
+          },
         ]
       },
+    // time
     {
       title: 'Created At',
       name: 'createdAt',
