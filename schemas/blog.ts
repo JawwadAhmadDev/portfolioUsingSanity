79927d5480d0d0af8033e67a9132df86b1d099f4
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-08-16 21:58:46 +0500

    sanity initialized successfuly

---
+++ schemas/blog.ts
@@ -0,0 +1,28 @@
+export default {
+  name: 'blog',
+  type: 'document',
+  title: 'Blog',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+    },
+    {
+      name: 'document',
+      type: 'string',
+      title: 'Document',
+    },
+    {
+      title: 'Launch Scheduled At',
+      name: 'launchAt',
+      type: 'datetime',
+      options: {
+        dateFormat: 'YYYY-MM-DD',
+        timeFormat: 'HH:mm',
+        timeStep: 15,
+        calendarTodayLabel: 'Today',
+      },
+    },
+  ],
+}
