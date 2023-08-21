813c75fbe5a24f7c5e88664a5dbf36b5b99e2351
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-08-21 17:13:47 +0500

    integration of sanity with next js 13

--- next-frontend/src/app/page.tsx
+++ next-frontend/src/app/page.tsx
@@ -1,9 +1,31 @@
-import Image from 'next/image'
+import { createClient } from "next-sanity";
 
-export default function Home() {
+export default async function Home() {
+  const blogs = await fetchData();
+  console.log("blogs", blogs.props);
   return (
     <main>
       <div>Hello world</div>
     </main>
-  )
+  );
+}
+
+const client = createClient({
+  projectId: "w8764j6a",
+  dataset: "production",
+  apiVersion: "v2021-10-21",
+  useCdn: false,
+});
+export async function fetchData() {
+  try {
+    const blogs = await client.fetch(`*[_type == "blog"]`);
+    return {
+      props: { blogs },
+    };
+  } catch (error) {
+    console.error("Error fetching blogs:", error);
+    return {
+      props: { blogs: [] }, // Return an empty array or some default value.
+    };
+  }
 }
